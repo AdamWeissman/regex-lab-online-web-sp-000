db616cc27011335149381ae829ed30a53ed15031
Adam Weissman <adam@comedytragedyepic.com>
2019-09-10 00:18:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[AEIOUaeiou]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -11,9 +11,38 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z]\W/)
 end
 
 def valid_phone_number?(phone)
 
 end
+
+=begin
+
+  
+require 'pry'
+
+def starts_with_a_vowel?(word)
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
+end
+
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w*ing\b/)
+end
+
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
+end
+
+def valid_phone_number?(phone)
+  phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+end
+
+
+
+=end
